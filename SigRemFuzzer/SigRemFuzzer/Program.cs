47e5a715878180f90901a1f7e55b313502708d5b
dennisbabkin.com <dennis_babkin@hotmail.com>
2021-04-24 19:34:56 -0700

    Update Program.cs

--- SigRemFuzzer/SigRemFuzzer/Program.cs
+++ SigRemFuzzer/SigRemFuzzer/Program.cs
@@ -1,4 +1,28 @@
-﻿using System;
+//  
+//    Binary File Digital Signature Remover App
+//    "Utility to remove digital code signature from binary PE files in Windows."
+//    Copyright (c) 2021 www.dennisbabkin.com
+//    
+//        https://dennisbabkin.com/sigremover
+//
+//        https://dennisbabkin.com/blog/?i=AAA10400
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//    
+//        https://www.apache.org/licenses/LICENSE-2.0
+//    
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//  
+//
+
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
